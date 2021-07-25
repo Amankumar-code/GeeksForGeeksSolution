e09622e46f32b4b13bf93577dae1c9179af08fda
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-07-26 02:15:14 +0530

    Create 6.Primality Test.cpp

---
+++ Track/1.Mathamatics/6.Primality Test.cpp
@@ -0,0 +1,12 @@
+class Solution{
+    public:
+        bool isPrime(int N)
+{
+    if(N<=1) return false;
+    for(int i=2;i<N;i++)
+        if(N%i==0)
+            return false;
+        return true;
+}
+
+};
