c3ddc82bb16f8ee9ebada4ec89440a1d4f5cf5ea
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-07-26 02:19:20 +0530

    Update 9.Multiplication_Under_Modulo.cpp

--- Track/1.Mathamatics/9.Multiplication_Under_Modulo.cpp
+++ Track/1.Mathamatics/9.Multiplication_Under_Modulo.cpp
@@ -1,11 +1,11 @@
 class Solution{
     public:
-        long long sumUnderModulo(long long a,long long b)
+        int multiplicationUnderModulo(long long a,long long b)
     {
-        // code here
+        //your code here
         int M=1000000007;
-    a = a%M;
-    b = b%M;
-    return (a+b)%M;
+    a=a%M;
+    b=b%M;
+    return (a*b)%M;
     }
 };
