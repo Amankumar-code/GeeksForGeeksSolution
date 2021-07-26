b271b3b5097b520c0f26dcfabbefd787d221a83d
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-07-26 12:22:18 +0530

    Create 9. Gray to Binary equivalent.cpp

---
+++ Track/2. Bit Magic/9. Gray to Binary equivalent.cpp
@@ -0,0 +1,18 @@
+class Solution{
+    
+    // function to convert a given Gray equivalent n to Binary equivalent.
+    public : 
+    int grayToBinary(int n)
+    {
+        
+        // Your code here
+        int res = n;
+    while (n > 0) 
+    {
+        n >>= 1;
+        res ^= n;
+    }
+    return res;
+        
+    }
+};
