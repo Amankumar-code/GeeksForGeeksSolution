687095a23658ee0c23194d44f4c05844d0cd7c99
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-07-30 22:47:27 +0530

    Create 15. Minimum Difference in an Array.py

---
+++ Track/5. Sorting/15. Minimum Difference in an Array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def MinimumDifference(self, arr, n):
+        arr = sorted(arr)
+        diff = 10**20
+     
+        for i in range(n-1):
+            if arr[i+1] - arr[i] < diff:
+                diff = arr[i+1] - arr[i]
+     
+        return diff
