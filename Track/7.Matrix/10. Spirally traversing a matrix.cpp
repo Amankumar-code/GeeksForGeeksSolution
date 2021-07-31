c1b7e0bb519fd742cc21b610772e0c4642fbcd60
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-07-31 12:31:24 +0530

    Create 10. Spirally traversing a matrix.cpp

---
+++ Track/7.Matrix/10. Spirally traversing a matrix.cpp
@@ -0,0 +1,47 @@
+class Solution
+{   
+    public: 
+    //Function to return a list of integers denoting spiral traversal of matrix.
+    vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c) 
+    {
+        // code here 
+         int T,B,L,R,dir;
+    T=0;
+    B=matrix.size()-1;
+    L=0;
+    R=matrix[0].size()-1;
+    dir=0;
+    int i;
+    vector<int> ans;
+    
+    while(T<=B && L<=R)
+    {
+        if(dir==0)
+        {
+            for(i=L;i<=R;i++)
+                ans.push_back(A[T][i]);
+            T++;
+        }
+        else if(dir==1)
+        {
+            for(i=T;i<=B;i++)
+                ans.push_back(A[i][R]);
+            R--;
+        }
+        else if(dir==2)
+        {
+            for(i=R;i>=L;i--)
+                ans.push_back(A[B][i]);
+            B--;
+        }
+        else if(dir==3)
+        {
+            for(i=B;i>=T;i--)
+                ans.push_back(A[i][L]);
+            L++;
+        }
+        dir=(dir+1)%4;
+    }
+    return ans;
+    }
+};
