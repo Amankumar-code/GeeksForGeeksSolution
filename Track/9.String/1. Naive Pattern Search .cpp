a1605c31b0895da9c9f70b2b7cba20e44fba01a5
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-07-31 13:35:34 +0530

    Create 1. Naive Pattern Search .cpp

---
+++ Track/9.String/1. Naive Pattern Search .cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+    public:
+    //Function to check if the given pattern exists in the given string or not.
+    bool search(string pat, string txt)
+{
+
+int n = txt.length();
+int m = pat.length();
+for(int i = 0; i < n; i++)
+{
+int j;
+for(j = 0; j < m; j++)
+{
+if(txt[i+j] != pat[j])
+break;
+}
+if(j == m)
+return true;
+}
+return false;
+}
+};
