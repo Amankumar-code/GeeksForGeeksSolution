65e18ec7dddb03676dc637476cb64c84bcff56e9
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-07-31 13:01:39 +0530

    Create 5. Print Non-Repeated Elements.cpp

---
+++ Track/8.Hashing/5. Print Non-Repeated Elements.cpp
@@ -0,0 +1,14 @@
+class Solution{
+  public:
+    vector<int> printNonRepeated(int arr[],int n)
+    {
+        unordered_map<int, int>m;
+        vector<int>v;
+        for(int i=0;i<n;i++)
+            m[arr[i]]++;
+        for(int i=0;i<n;i++)
+            if(m[arr[i]]==1)
+                v.push_back(arr[i]);
+        return v;
+    }
+};
