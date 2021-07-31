2aef2ad1d0231b5154ee8a86c3f4c1f7e7b2cb43
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-07-31 13:44:07 +0530

    Create 11. Repeating Character - First Appearance Leftmost.cpp

---
+++ Track/9.String/11. Repeating Character - First Appearance Leftmost.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+    public:
+    //Function to find repeated character whose first appearance is leftmost.
+    int repeatedCharacter (string str) 
+{ 
+    unordered_set<int> s;
+    int pos=-1;
+    
+    for(int i=str.length()-1;i>=0;i--){
+        if(s.find(str[i]) != s.end()){
+            pos = i;
+        }
+        
+        s.insert(str[i]);
+    }
+    
+    return pos;
+} 
+};
