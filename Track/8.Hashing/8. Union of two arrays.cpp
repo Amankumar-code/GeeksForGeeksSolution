61b924cf88940f9f7621ac6d87612b1841e27642
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-07-31 13:12:01 +0530

    Create 8. Union of two arrays.cpp

---
+++ Track/8.Hashing/8. Union of two arrays.cpp
@@ -0,0 +1,13 @@
+class Solution
+   {
+    public:
+    int doUnion(int a[], int n, int b[], int m)  
+    {
+        unordered_set<int>s;
+        for(int i=0;i<n;++i)
+            s.insert(a[i]);
+        for(int i=0;i<m;++i)
+            s.insert(b[i]);
+        return s.size();
+    }
+};
