0a9d8d969b3cb3e9ee360f297255f6dc973e0c72
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-08-01 22:16:42 +0530

    Create 3. Implement Queue using Linked List.cpp

---
+++ Track/12.Queue/3. Implement Queue using Linked List.cpp
@@ -0,0 +1,23 @@
+void MyQueue:: push(int x)
+{
+        QueueNode *temp = new QueueNode(x);
+        if (front == NULL){
+            front = rear = temp;
+            return;        
+        }
+        rear->next = temp;
+        rear = temp;
+}
+
+int MyQueue :: pop()
+{
+        if (front == NULL)
+            return -1;
+        QueueNode *head1 = front;
+        front = front->next;
+        if (front == NULL)
+            rear = NULL;
+        int res = head1->data;
+        delete head1;
+        return res;
+}
