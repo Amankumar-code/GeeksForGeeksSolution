5d8c300a5961bb320808b97767c5f81fc5ad20b6
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-08-01 21:15:14 +0530

    Update 10. Spirally traversing a matrix.cpp

--- Track/7.Matrix/10. Spirally traversing a matrix.cpp
+++ Track/7.Matrix/10. Spirally traversing a matrix.cpp
@@ -1,47 +1,34 @@
 class Solution
 {   
     public: 
-    //Function to return a list of integers denoting spiral traversal of matrix.
     vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c) 
     {
-        // code here 
-         int T,B,L,R,dir;
-    T=0;
-    B=matrix.size()-1;
-    L=0;
-    R=matrix[0].size()-1;
-    dir=0;
-    int i;
-    vector<int> ans;
-    
-    while(T<=B && L<=R)
-    {
-        if(dir==0)
+        vector<int> v;
+        int top=0, bottom=r-1, left=0, right=c-1;
+        while(top<=bottom && left<=right)
         {
-            for(i=L;i<=R;i++)
-                ans.push_back(A[T][i]);
-            T++;
+            for(int i=left; i<=right; i++)
+                v.push_back(matrix[top][i]);
+            top++;
+            
+            for(int i=top; i<=bottom; i++)
+                v.push_back(matrix[i][right]);
+            right--;
+            
+            if(top <= bottom)
+            {
+                for(int i = right; i >= left; i--)
+                    v.push_back(matrix[bottom][i]);
+                bottom--;
+            }
+            
+            if(left <= right)
+            {
+                for(int i = bottom; i >= top; i--)
+                    v.push_back(matrix[i][left]);
+                left++;
+            }
         }
-        else if(dir==1)
-        {
-            for(i=T;i<=B;i++)
-                ans.push_back(A[i][R]);
-            R--;
-        }
-        else if(dir==2)
-        {
-            for(i=R;i>=L;i--)
-                ans.push_back(A[B][i]);
-            B--;
-        }
-        else if(dir==3)
-        {
-            for(i=B;i>=T;i--)
-                ans.push_back(A[i][L]);
-            L++;
-        }
-        dir=(dir+1)%4;
-    }
-    return ans;
+        return v;
     }
 };
