08abfd1079865be31bb8c585f9e3305a82ce40da
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-08-01 21:25:13 +0530

    Update 10. Hashing for pair - 2.java

--- Track/8.Hashing/10. Hashing for pair - 2.java
+++ Track/8.Hashing/10. Hashing for pair - 2.java
@@ -1,34 +1,14 @@
-class Gfg
+class Geeks
 {
-    public static void main (String[] args) {
+    public static int sumExists(int arr[], int sizeOfArray, int sum)
+    {
+        HashSet<Integer> hs = new HashSet<>();
         
-        //Taking input using class Scanner
-        Scanner sc = new Scanner(System.in);
-        
-        //Taking total count of testcases
-        int testcases = sc.nextInt();
-        
-        
-        while(testcases-- > 0)
-        {
-            //taking size of array
-            int N = sc.nextInt();
-            int arr[] =  new int[N];  //array declaration
-            
-            for(int i = 0; i < N; i++)
-             arr[i] = sc.nextInt(); //Input the array
-             
-            int sum = 0; 
-            
-            //taking value of sum
-            sum = sc.nextInt();
-            
-            //Creating an object of class Geeks
-            Geeks obj = new Geeks();
-            
-            //Calling sumExists method of class Geeks
-            //and printing the result
-            System.out.println(obj.sumExists(arr, N, sum));
+        for(int i=0;i<sizeOfArray;i++){
+            if(hs.contains(sum-arr[i]))
+                return 1;
+            hs.add(arr[i]);
         }
+        return 0;
     }
 }
