48ba54d7fe1c281453a8c0686e5af4b9bfbecd0d
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-08-01 22:17:27 +0530

    Create 5. Queue using two Stacks.cpp

---
+++ Track/12.Queue/5. Queue using two Stacks.cpp
@@ -0,0 +1,25 @@
+void StackQueue :: push(int x)
+{
+    // Your Code
+    s1.push(x);
+}
+
+//Function to pop an element from queue by using 2 stacks.
+int StackQueue :: pop()
+{
+        // Your Code 
+        if(s1.empty() and s2.empty()) return -1;
+        
+        if(s2.empty())
+        {
+            while(!s1.empty())
+            {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        
+        int temp= s2.top();
+        s2.pop();
+        return temp;
+}
