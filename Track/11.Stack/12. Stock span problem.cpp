e90856458f14285b3558686097fac2f5c80ccf2b
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-08-01 21:53:54 +0530

    Create 12. Stock span problem.cpp

---
+++ Track/11.Stack/12. Stock span problem.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+    public:
+    //Function to calculate the span of stockâ€™s price for all n days.
+    vector <int> calculateSpan(int price[], int n)
+    {
+       vector<int>res(n, 1);
+       stack<int>st;
+       for(int i=0;i<n;i++)
+       {
+           while(!st.empty() and price[st.top()]<=price[i])
+           {
+               st.pop();
+           }
+           res[i]= st.empty()? i+1 : (i-st.top());
+           st.push(i);
+       }
+       return res;
+    }
+};
