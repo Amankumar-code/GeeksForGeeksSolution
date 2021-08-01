79a07fd79bac906ff174fbf5aa444937b3d37e60
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-08-01 22:26:43 +0530

    Update 16. Allocate minimum number of pages.cpp

--- Track/Searching/16. Allocate minimum number of pages.cpp
+++ Track/Searching/16. Allocate minimum number of pages.cpp
@@ -1,33 +1,71 @@
 class Solution 
 {
     public:
-    //Function to find minimum number of pages.
     bool isPossible(int arr[], int n, int m, int curr_min)
 {
     int studentsRequired = 1;
     int curr_sum = 0;
  
-    // iterate over all books
     for (int i = 0; i < n; i++)
     {
         
         if (arr[i] > curr_min)
             return false;
-
+ 
+       
         if (curr_sum + arr[i] > curr_min)
         {
             
             studentsRequired++;
-
+ 
+            
             curr_sum = arr[i];
  
+           
             if (studentsRequired > m)
                 return false;
         }
  
-        // else update curr_sum
+        
         else
             curr_sum += arr[i];
     }
     return true;
 }
+ 
+int findPages(int arr[], int n, int m)
+{
+    long long sum = 0;
+ 
+    
+    if (n < m)
+        return -1;
+ 
+    
+    for (int i = 0; i < n; i++)
+        sum += arr[i];
+ 
+    
+    int start = 0, end = sum;
+    int result = INT_MAX;
+ 
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
+        if (isPossible(arr, n, m, mid))
+        {
+              result = mid;
+ 
+            
+            end = mid - 1;
+        }
+ 
+        else
+           
+            start = mid + 1;
+    }
+ 
+    return result;
+}
+ 
+};
