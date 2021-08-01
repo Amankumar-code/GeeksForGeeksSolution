96333b2d0b66cc6e4661db101b9b8b65b670cefb
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-08-01 22:15:31 +0530

    Create 2. Operations on Queue.cpp

---
+++ Track/12.Queue/2. Operations on Queue.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+    public:
+    void enqueue(queue<int> &q,int x)
+    {
+       q.push(x);
+    }
+    void dequeue(queue<int> &q)
+    {
+        q.pop();
+    }
+    int front(queue<int> &q)
+    {
+        return q.front();
+    }
+    
+    string find(queue<int> q, int x)
+    {
+        int len = q.size();
+        for(int i =0 ; i< len ; i++)
+        {
+            int temp=q.front();
+            if(temp==x)
+            {
+            return "Yes";
+            }
+            q.pop();
+        }
+        return "No";
+    }
+};
