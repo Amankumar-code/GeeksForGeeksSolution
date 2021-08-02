dad067761fd0025baf6a08a816dfb766cfeafcf3
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-08-02 19:15:34 +0530

    Create 2. Heap Sort.cpp

---
+++ Track/16.Heap/2. Heap Sort.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+    public:
+    //Heapify function to maintain heap property.
+    void heapify(int arr[], int n, int i)  
+    {
+      // Your Code Here
+    }
+
+    public:
+    //Function to build a Heap from array.
+    void buildHeap(int arr[], int n)  
+    { 
+    // Your Code Here
+    }
+
+    
+    public:
+    //Function to sort an array using Heap Sort.
+    void heapSort(int arr[], int n)
+    {
+        //code here
+    }
+};
