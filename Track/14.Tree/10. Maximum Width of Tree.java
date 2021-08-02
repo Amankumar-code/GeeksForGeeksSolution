94e7ded1288b1fd6acf1cf1ebd61611b49dd7ff6
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-08-02 17:32:56 +0530

    Create 10. Maximum Width of Tree.java

---
+++ Track/14.Tree/10. Maximum Width of Tree.java
@@ -0,0 +1,26 @@
+class Solution 
+{
+    int getMaxWidth(Node root)
+    {
+        if(root == null)
+            return 0;
+            
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        int res = 0;
+        
+        while(!q.isEmpty()){
+            int count = q.size();
+            res = Math.max(res,count);
+            
+            for(int i=0;i<count;i++){
+                Node curr = q.poll();
+                if(curr.left != null)
+                    q.add(curr.left);
+                if(curr.right != null)
+                    q.add(curr.right);
+            }
+        }
+        return res;
+    }	
+}
