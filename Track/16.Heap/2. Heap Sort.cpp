8465ae415a449b4321194d149524e8ec697ac549
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-08-02 22:14:09 +0530

    Update 2. Heap Sort.cpp

--- Track/16.Heap/2. Heap Sort.cpp
+++ Track/16.Heap/2. Heap Sort.cpp
@@ -1,24 +1,42 @@
 class Solution
 {
     public:
-    //Heapify function to maintain heap property.
     void heapify(int arr[], int n, int i)  
     {
-      // Your Code Here
+        int largest = i;
+        int left =( 2 * i) +1;
+        int right =( 2 * i) +2;
+        
+        if( left < n && arr [largest] < arr [ left ] )
+            largest = left ;
+        
+        if( right < n && arr[ largest] < arr[ right ])
+            largest= right;
+        
+        if( largest != i)
+        {
+            swap (arr[i] , arr[largest]);
+            heapify (arr , n , largest);
+        }  
     }
-
     public:
-    //Function to build a Heap from array.
     void buildHeap(int arr[], int n)  
-    { 
-    // Your Code Here
+    {
+        int pos = (n-1) / 2 ;
+        
+        for(int i = pos ; i>= 0 ; i-- )
+        {
+            heapify( arr , n , i ) ;
+        }
     }
-
-    
     public:
-    //Function to sort an array using Heap Sort.
     void heapSort(int arr[], int n)
     {
-        //code here
+       buildHeap(arr,n);
+       for (int i = n - 1; i > 0; i--)
+        {
+           swap(arr[0], arr[i]);
+           heapify(arr, i, 0);
+        }
     }
 };
