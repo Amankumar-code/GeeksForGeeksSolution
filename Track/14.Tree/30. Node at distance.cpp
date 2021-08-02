3525507816442d84d27a9721508485e0e0f25f91
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-08-02 22:25:55 +0530

    Update 30. Node at distance.cpp

--- Track/14.Tree/30. Node at distance.cpp
+++ Track/14.Tree/30. Node at distance.cpp
@@ -1,83 +1,21 @@
-int printKDistantfromLeaf(Node* node, int k);
-
-// Function to Build Tree
-Node* buildTree(string str)
+bool funcount(Node*root,int k)
 {
-    // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
-        return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for(string str; iss >> str; )
-        ip.push_back(str);
-
-    // Create the root of the tree
-    Node* root = newNode(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while(!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-
-        // Get the current node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if(currVal != "N") {
-
-            // Create the left child for the current node
-            currNode->left = newNode(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if(i >= ip.size())
-            break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if(currVal != "N") {
-
-            // Create the right child for the current node
-            currNode->right = newNode(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
+if(root==NULL)
+return false;
+if(!root->left&&!root->right&&!k)
+{
+return true;
 }
+return funcount(root->left,k-1)||funcount(root->right,k-1);
+}
+int printKDistantfromLeaf(Node* root, int k)
+{
+int count=0;
+if(root==NULL)
+return 0;
+if(funcount(root,k))
+return 1+printKDistantfromLeaf(root->left,k)+printKDistantfromLeaf(root->right,k);
+else
+return printKDistantfromLeaf(root->left,k)+printKDistantfromLeaf(root->right,k);
 
-
-int main() {
-    int t;
-    string tc;
-    getline(cin, tc);
-    t=stoi(tc);
-    while(t--)
-    {
-        string s, ks;
-        getline(cin, s);
-        Node* root = buildTree(s);
-        getline(cin, ks);
-        int k=stoi(ks);
-        cout<<printKDistantfromLeaf(root, k)<<endl;
-    }
-    return 0;
 }
