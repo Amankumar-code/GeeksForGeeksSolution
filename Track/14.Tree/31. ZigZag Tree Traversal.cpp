765250431f93ec051742cf5910e2155786ba8418
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-08-02 22:27:52 +0530

    Update 31. ZigZag Tree Traversal.cpp

--- Track/14.Tree/31. ZigZag Tree Traversal.cpp
+++ Track/14.Tree/31. ZigZag Tree Traversal.cpp
@@ -1,56 +1,41 @@
-Node* buildTree(string str)
-{   
-    // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
-    // string after spliting by space
-    vector<string> ip;
-    
-    istringstream iss(str);
-    for(string str; iss >> str; )
-        ip.push_back(str);
-        
-    
-    Node* root = new Node(stoi(ip[0]));
-        
-    queue<Node*> queue;
-    queue.push(root);
-        
-    int i = 1;
-    while(!queue.empty() && i < ip.size()) {
-            
-        Node* currNode = queue.front();
-        queue.pop();
-            
-        string currVal = ip[i];
-            
-        
-        if(currVal != "N") {
-            currNode->left = new Node(stoi(currVal));
-                
-            
-            queue.push(currNode->left);
+vector <int> zigZagTraversal(Node* root)
+{
+    vector<int> zz;
+
+	if (root == NULL)
+	    return zz;
+
+    int direction = 1;
+
+    stack<Node *> currLevel;
+    currLevel.push(root);
+
+    stack<Node *> nextLevel;
+
+    while (!currLevel.empty()) {
+
+        Node *temp = currLevel.top();
+        currLevel.pop();
+
+        if (temp) {
+
+            zz.emplace_back(temp -> data);
+
+            if (direction) {
+                if (temp -> left) nextLevel.push(temp -> left);
+                if (temp -> right) nextLevel.push(temp -> right);
+            }
+            else {
+                if (temp -> right) nextLevel.push(temp -> right);
+                if (temp -> left) nextLevel.push(temp -> left);
+            }
         }
-            
-        
-        i++;
-        if(i >= ip.size())
-            break;
-        currVal = ip[i];
-            
-        
-        if(currVal != "N") {
-                
-            
-            currNode->right = new Node(stoi(currVal));
-                
-            
-            queue.push(currNode->right);
+
+        if (currLevel.empty()) {
+            swap(currLevel, nextLevel);
+            direction = 1 - direction;
         }
-        i++;
     }
-    
-    return root;
+
+    return zz;
 }
