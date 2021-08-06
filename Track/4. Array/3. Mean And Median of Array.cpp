908bd9ee372fb376d5bb78a4b3f2b6d030730442
Aman Kumar <69410344+Amankumar-code@users.noreply.github.com>
2021-08-06 20:52:11 +0530

    Update 3. Mean And Median of Array.cpp

--- Track/4. Array/3. Mean And Median of Array.cpp
+++ Track/4. Array/3. Mean And Median of Array.cpp
@@ -2,22 +2,26 @@ class Solution{
     public:
     //Function to find median of the array elements.
     int median(int A[],int N)
-{
-    
-    sort(A,A+N); 
-    if(N%2==0){
-        return (A[N/2]+A[(N/2)-1])/2;
-       
+    {
+        
+        sort(A,A+N);
+        
+        //your code here
+        //If median is in fraction then convert it to integer and return
+    if (N % 2 != 0)
+        return A[N / 2];
+    return (A[(N - 1) / 2] + A[N / 2]) / 2.0;
     }
-    else  return A[N/2];
-    
-   
-}
-int mean(int A[],int N)
-{  int sum=0;
-    for(int i=0;i<N;i++){
-     sum=sum+A[i];
+    //Function to find mean of the array elements.
+    int mean(int A[],int N)
+    {
+        //your code here
+        int sum=0;
+        
+        for(int i=0;i<N;i++){
+            sum+=A[i];
+        }
+        
+        return sum/N;
     }
-    return sum/N;
-}
 };
